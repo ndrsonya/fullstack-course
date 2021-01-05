671460499378a105da25a8f87f78d312219eee83
ndrsonya <ViktorAnderson@MBP-Viktor.home>
2021-01-05 17:45:12 +0200

    part3 added

---
+++ part3/phonebook_backend/build/precache-manifest.eebc07c73d74295ef32424bfdf5c4e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5948b03b632aba8f986e3dd269af611f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9282520d65d86ad81e4",
+    "url": "/static/css/main.dc2c9a58.chunk.css"
+  },
+  {
+    "revision": "29d90cdbc36696dfc3c1",
+    "url": "/static/js/2.d26cb849.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d26cb849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9282520d65d86ad81e4",
+    "url": "/static/js/main.1b2a0869.chunk.js"
+  },
+  {
+    "revision": "194d2a893d70517d6cc2",
+    "url": "/static/js/runtime-main.8f096503.js"
+  }
+]);
---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,222 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+const Person = require('./models/person')
+const bodyParser = require('body-parser')
+
+//app.use(express.json())
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static('build'))
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'))
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    const d = new Date()
+    Person.count(function (err, count) {
+        if (err) {
+            console.log(err)
+        }
+        response.send('<div><p>Phonebook has info for ' + count + ' persons</p>' + d + '<p></p></div>')
+    })
+
+})
+
+//const Person = mongoose.model('Person', personSchema)
+
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+    person
+        .save()
+        .then(savedPerson =>  savedPerson.toJSON())
+        .then(savedAndFormattedPerson => {
+            response.json(savedAndFormattedPerson)
+        })
+        .catch(error => {
+            next(error)
+            return error
+        })
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+/***********CODE FOR PREVIOUS EXERCISES***************
+
+************GENERATE ID***************
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+************PERSONS ARRAY***************
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+************GET ENDPOINTS***************
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  const sameName = persons.filter(person => person.name === body.name);
+
+  if ((!body.name) || (!body.number)) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  } else if (sameName.length > 0) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
+************DELETE ENDPOINT***************
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})*/
